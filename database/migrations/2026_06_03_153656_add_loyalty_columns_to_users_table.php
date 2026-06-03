deccb5f2538123604ba21e003b954f998b412f0b
sheydaashoorian <sheydaashoorian@gmail.com>
2026-06-03 19:20:04 +0330

    add add_loyalty_columns_to_users_table migration

---
+++ database/migrations/2026_06_03_153656_add_loyalty_columns_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->integer('blocked_points')->default(0)->after('points_balance');
+                
+
+            $table->integer('pending_points')->default(0)->after('blocked_points');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
